63b78dc943ba64d1d622cf9efe76f7ca57868fcb
Cuong Nguyen <cuong.nguyen@apps-cyclone.com>
2026-08-10 03:36:52 +0000

    add icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -95,6 +95,19 @@ export function ImageIcon(props: {
   );
 }
 
+export function CameraIcon(props: {
+  size?: number;
+  className?: string;
+  gradient?: boolean;
+}) {
+  return (
+    <Icon {...props}>
+      <path d="M3 8a2 2 0 0 1 2-2h2l1.5-2h7L19 6h0a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+      <circle cx="12" cy="13" r="3.5" />
+    </Icon>
+  );
+}
+
 export function ChevronRightIcon(props: {
   size?: number;
   className?: string;
